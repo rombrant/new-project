f75272eeba92f2fc747bda130c242fe030af4a3e
Roman Safin <gordonfreeman2332@gmail.com>
2019-04-26 11:22:34 +0300

    yandex map marker src changed (4)

--- src/main.js
+++ src/main.js
@@ -2,7 +2,8 @@ import "./styles/main.pcss";
 if (process.env.NODE_ENV === "development") {
   require("file-loader!./index.pug");
 }
-
+function requireAll(r) { r.keys().forEach(r); }
+requireAll(require.context('images/', true));
 
 import "./scripts/maps.js";
 import "./scripts/hide-menu.js";
